9ed1d352e4d90fe524e6b2331d6229d8a676aab5
shuhaozhangtony <shuhao_zhang@sutd.edu.sg>
2024-10-10 16:54:57 +0800

    code cleanup. Add simple test units.

--- cmake/EnableTests.cmake
+++ cmake/EnableTests.cmake
@@ -1,8 +1,5 @@
 # cmake/EnableTests.cmake
 
-option(ENABLE_UNIT_TESTS "Enable unit tests" ON)
-message(STATUS "Enable testing: ${ENABLE_UNIT_TESTS}")
-
 if (ENABLE_UNIT_TESTS)
     # Add tests that depend on the CANDY library
     add_subdirectory(test)
