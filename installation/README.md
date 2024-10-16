2efa62b63d37215b7128cb617eea43ad9c01c218
tonyskyzeng <292224750@qq.com>
2024-10-16 19:31:48 +0800

    6. add native support

---
+++ installation/README.md
@@ -0,0 +1,4 @@
+# Structures
+   - docker: The set-ups on docker environment
+   - nativeUbuntu: The set-ups native ubuntu, only tested in Ubuntu 22.04
+```
---
+++ installation/nativeUbuntu/tryInstallCuda.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+get_cuda_version() {
+    # Check if nvcc is available
+    if command -v /usr/local/cuda/bin/nvcc &> /dev/null; then
+        # Use nvcc to extract the version number
+        cuda_version=$(/usr/local/cuda/bin/nvcc --version | grep "release" | grep -oP 'release \K[0-9]+\.[0-9]+')
+        echo "CUDA is already installed."
+        exit 1
+    else
+        echo "CUDA is not installed or nvcc is not in your PATH, try to install"
+        sudo apt install -y wget
+        wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+        sudo dpkg -i cuda-keyring_1.1-1_all.deb
+        sudo apt-get install -y nvidia-gds-12-5 cuda-12-5 cuda-toolkit-12-5
+        echo "Please go back to start.sh after the following reboot"
+        sudo reboot
+        exit 1
+    fi
+    echo $cuda_version
+}
+# Check if there is an NVIDIA GPU installed
+if lspci | grep -i 'nvidia' > /dev/null; then
+    echo "NVIDIA GPU is installed."
+    get_cuda_version()
+else
+    echo "No NVIDIA GPU detected, go to the cpu-only set up."
+    ./startWithoutCuda.sh
+    exit 1
+fi
