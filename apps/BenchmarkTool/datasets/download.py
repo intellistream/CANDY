7d038eb9db076efff5f04c19eb6b00218aa13959
AmuroEita <1071307515@qq.com>
2024-10-21 10:06:33 +0000

    datasets scripts

---
+++ apps/BenchmarkTool/datasets/download.py
@@ -0,0 +1,11 @@
+import download_tar_gz
+import download_hdf5
+
+print(download_hdf5.__file__)
+
+def main():
+  download_tar_gz.main() # fvecs datasets 
+  download_hdf5.main() # hdf5 datasets
+
+if __name__ == "__main__":
+    main()
