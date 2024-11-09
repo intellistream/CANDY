e37871cc3231015b9da1c7121ae2dd7802ed7a26
shuhao_zhang <shuhao_zhang@sutd.edu.sg>
2024-11-10 00:58:52 +0800

    replace some sources at chinese mirrors.
    update the auto_env_setup.sh to remove huggingface logging as chinese mirrors do not support that, unfortunately.

--- cmake/EnableTests.cmake
+++ cmake/EnableTests.cmake
@@ -7,7 +7,7 @@ if (${ENABLE_UNIT_TESTS})
 
     FetchContent_Declare(
             Catch2
-            GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+            GIT_REPOSITORY https://gitcode.com/gh_mirrors/ca/Catch2.git
             GIT_TAG v3.4.0
     )
     FetchContent_MakeAvailable(Catch2)
