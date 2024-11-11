b13fe4bc66f126c38935d5096dc1c0e10e314ed3
zyt <3033439590@qq.com>
2024-11-11 09:17:35 +0800

    Fix errors when using dot product similarity

--- src/Algorithms/KNN/KNNSearch.cpp
+++ src/Algorithms/KNN/KNNSearch.cpp
@@ -113,9 +113,12 @@ std::vector<torch::Tensor> CANDY_ALGO::KnnSearch::searchTensor(
 
   // For each query, retrieve the top-k nearest neighbors
   for (int64_t i = 0; i < q.size(0); ++i) {
+    std::cout << "Shape of distances[i]: " << distances[i].sizes() << std::endl;
     // Find the indices of the top-k smallest distances for the current query row
     auto topk = std::get<1>(distances[i].topk(
-        k, /*largest=*/false));  // Get indices of top-k closest neighbors
+        k,/*dim=*/0,/*largest=*/true));  // Get indices of top-k closest neighbors
+    //when using dot product similarity largest=true;
+    //when using L2 , largest=false
 
     // Store the indices of top-k nearest neighbors for this query row
     results.push_back(topk);
