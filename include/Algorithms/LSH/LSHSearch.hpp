a467545665030d11b6f6574ed27bde8234f6dd40
MingqiWang-coder <15751853706@163.com>
2024-11-11 17:53:42 +0800

    Migrate a basic LSH algorithm 2.0 (#85)
    * Migrate a basic LSH algorithm 2.0
    
    * chore: Automatically formatted code using clang-format
    
    ---------
    
    Co-authored-by: MingqiWang-coder <1403429057@qq.com>
    Co-authored-by: MingqiWang-coder <MingqiWang-coder@users.noreply.github.com>
    Co-authored-by: Shuhao Zhang (Tony) <tonyzhang19900609@gmail.com>

---
+++ include/Algorithms/LSH/LSHSearch.hpp
@@ -0,0 +1,70 @@
+/*
+* Copyright (C) 2024 by the INTELLI team
+ * Created on: 24-11-5 下午2:07
+ * Description: ${DESCRIPTION}
+ */
+
+#ifndef LSHSEARCH_HPP
+#define LSHSEARCH_HPP
+
+#include <torch/torch.h>
+#include <Algorithms/ANNSBase.hpp>
+#include <memory>
+#include <random>
+#include <unordered_map>
+#include <vector>
+
+namespace CANDY_ALGO {
+
+class LshSearch : public ANNSBase {
+ protected:
+  INTELLI::ConfigMapPtr myCfg = nullptr;
+
+ public:
+  ~LshSearch() override = default;
+
+  // Constructor with vector dimensions and number of planes
+  explicit LshSearch(size_t Dimensions, size_t NumPlanes);
+
+  bool setConfig(INTELLI::ConfigMapPtr cfg) override;
+
+  void reset() override;
+
+  bool insertTensor(const torch::Tensor& t) override;
+
+  bool deleteTensor(torch::Tensor& t, int64_t k = 1) override;
+
+  bool reviseTensor(torch::Tensor& t, torch::Tensor& w) override;
+
+  std::vector<torch::Tensor> searchTensor(const torch::Tensor& q,
+                                          int64_t k) override;
+
+ private:
+  size_t Dimensions;
+  size_t GlobalIndexCounter = 0;
+
+  // Hash table (unordered_map) where each bucket corresponds to a map of tensors
+  std::unordered_map<std::string, std::unordered_map<int64_t, torch::Tensor>>
+      Index;
+
+  // Store hyperplane information
+  std::vector<torch::Tensor> RandomHyperplanes;
+
+  // Store nearby bucket information for each tensor
+  std::vector<std::vector<std::pair<float, std::string>>> nearbyBuckets;
+
+  // Generate random hyperplanes for hashing
+  void GenerateRandomHyperplanes(size_t NumPlanes);
+
+  // Hash function to map tensors to hash buckets
+  std::string HashFunction(const torch::Tensor& t);
+
+  // Hamming distance calculation for two binary strings
+  int HammingDistance(const std::string& str1, const std::string& str2);
+};
+
+}  // namespace CANDY_ALGO
+
+typedef std::shared_ptr<CANDY_ALGO::LshSearch> LSHSearchPtr;
+
+#endif  // LSHSEARCH_HPP
---
+++ test/AlgorithmsTests/LSHTests.cpp
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2024 by the INTELLI team
+ * Created on: 24-11-6 下午3:28
+ * Description: ${DESCRIPTION}
+ */
+
+#include <torch/torch.h>
+#include <Algorithms/LSH/LSHSearch.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+using namespace CANDY_ALGO;
+
+TEST_CASE("LSH Search Tests") {
+  // Set the dimensions and number of hyperplanes
+  size_t Dimensions = 10;
+  size_t NumPlanes = 10;
+
+  // Insert/Search/Delete test
+  SECTION("Insert/Search/Delete Tensor") {
+    LshSearch lsh(Dimensions, NumPlanes);
+    torch::Tensor Data = torch::randn({20000, Dimensions});
+    REQUIRE(lsh.insertTensor(Data));
+
+    torch::Tensor SearchQuery = Data.slice(0, 0, 2);
+    std::cout << "The tensors to be queried (first 2 rows): " << SearchQuery
+              << std::endl;
+
+    auto Results = lsh.searchTensor(SearchQuery, 5);
+    torch::Tensor result;
+
+    for (int m = 0; m < Results.size(); ++m) {
+      result = Results[m];
+      std::cout << "Row[" << m
+                << "] The most similar 5 tensors before deletion:" << std::endl;
+      for (int j = 0; j < result.size(0); ++j) {
+        int64_t id = result[j][0].item<int64_t>();
+        torch::Tensor row = Data[id].unsqueeze(0);
+        std::cout << "id " << id << ": " << row << std::endl;
+      }
+      std::cout << std::endl;
+    }
+    REQUIRE(Results.size() == 2);
+    REQUIRE(Results[0].size(0) == 5);
+
+    REQUIRE(lsh.deleteTensor(SearchQuery, 2));
+
+    Results = lsh.searchTensor(SearchQuery, 5);
+
+    for (int m = 0; m < Results.size(); ++m) {
+      result = Results[m];
+      std::cout << "Row[" << m
+                << "] The most similar 5 tensors after removing:" << std::endl;
+      for (int j = 0; j < result.size(0); ++j) {
+        int64_t id = result[j][0].item<int64_t>();
+        torch::Tensor row = Data[id].unsqueeze(0);
+        std::cout << "id " << id << ": " << row << std::endl;
+      }
+      std::cout << std::endl;
+    }
+
+    REQUIRE(Results.size() == 2);
+    REQUIRE(Results[0].size(0) == 5);
+  }
+
+  SECTION("Revise Tensor") {
+    LshSearch lsh(Dimensions, NumPlanes);
+    torch::Tensor Data = torch::randn({20000, Dimensions});
+    REQUIRE(lsh.insertTensor(Data));
+
+    torch::Tensor reviseQuery = Data.slice(0, 0, 3);
+    torch::Tensor NewTensor = torch::randn({3, Dimensions});
+
+    std::cout << "Primitive tensors: " << reviseQuery << std::endl;
+    std::cout << "New Tensors: " << NewTensor << std::endl;
+
+    REQUIRE(lsh.reviseTensor(reviseQuery, NewTensor));
+
+    Data.slice(0, 0, 3) = NewTensor;
+
+    auto Results = lsh.searchTensor(NewTensor, 5);
+    for (int m = 0; m < Results.size(); ++m) {
+      auto result = Results[m];
+      std::cout << "Row[" << m << "] Query after modifying the tensors"
+                << std::endl;
+      for (int j = 0; j < result.size(0); ++j) {
+        int64_t id = result[j][0].item<int64_t>();
+        torch::Tensor row = Data[id].unsqueeze(0);
+        std::cout << "id " << id << ": " << row << std::endl;
+      }
+      std::cout << std::endl;
+    }
+
+    REQUIRE(Results.size() == 3);
+    REQUIRE(Results[0].size(0) == 5);
+  }
+}
