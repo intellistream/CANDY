3d23ef18b4693ef8cd7bf926a0910ac7c389bae5
KimmoZAG <995496585@qq.com>
2024-11-21 20:28:37 +0800

    Unified Python interface and rewriting benchmarks(Python)

--- python_bindings/pybind11_bindings/vectordb_pybind.cpp
+++ python_bindings/pybind11_bindings/vectordb_pybind.cpp
@@ -7,9 +7,13 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <torch/extension.h>
+#include <Utils/ConfigMap.hpp>
 #include <Algorithms/KNN/KNNSearch.hpp>
+#include <DataLoader/DataLoaderTable.hpp>
 #include <Core/vector_db.hpp>  // Corrected include to match header guard and existing header file
 namespace py = pybind11;
+using namespace INTELLI;
+using namespace std;
 
 PYBIND11_MODULE(pycandy, m) {
 
@@ -53,4 +57,69 @@ PYBIND11_MODULE(pycandy, m) {
            py::arg("tensor"))
 
       .def("process_streaming_queue", &VectorDB::process_streaming_queue);
+
+  // m.def("createDataLoader", &creatDataLoader,
+  //     "A function to create new data loader by name tag");
+  //
+  // py::class_<CANDY_ALGO::AbstractDataLoader,
+  //          std::shared_ptr<CANDY_ALGO::AbstractDataLoader>>(
+  //   m, "AbstractDataLoader")
+  //     .def(py::init<>())
+  //     .def("setConfig", &CANDY_ALGO::AbstractDataLoader::setConfig)
+  //     .def("getData", &CANDY_ALGO::AbstractDataLoader::getData)
+  //     .def("getDataAt", &CANDY_ALGO::AbstractDataLoader::getDataAt)
+  //     .def("getQuery", &CANDY_ALGO::AbstractDataLoader::getQuery);
+
+  py::class_<CANDY_ALGO::AbstractDataLoader, std::shared_ptr<CANDY_ALGO::AbstractDataLoader>>(m, "AbstractDataLoader")
+    .def(py::init<>())
+    .def("setConfig", &CANDY_ALGO::AbstractDataLoader::setConfig)
+    .def("getData", &CANDY_ALGO::AbstractDataLoader::getData)
+    .def("getDataAt", &CANDY_ALGO::AbstractDataLoader::getDataAt)
+    .def("getQuery", &CANDY_ALGO::AbstractDataLoader::getQuery);
+
+  py::class_<CANDY_ALGO::DataLoaderTable, std::shared_ptr<CANDY_ALGO::DataLoaderTable>>(m, "DataLoaderTable")
+      .def(py::init<>())
+      .def("register_new_data_loader",
+           &CANDY_ALGO::DataLoaderTable::registerNewDataLoader)
+      .def("find_data_loader", &CANDY_ALGO::DataLoaderTable::findDataLoader);
+
+  py::class_<ConfigMap, std::shared_ptr<ConfigMap>>(m, "ConfigMap")
+      .def(py::init<>()) // Constructor
+      .def("edit_u64", static_cast<void (ConfigMap::*)(const std::string&, uint64_t)>(&ConfigMap::edit),
+             py::arg("key"), py::arg("value"))
+      .def("edit_i64", static_cast<void (ConfigMap::*)(const std::string&, int64_t)>(&ConfigMap::edit),
+             py::arg("key"), py::arg("value"))
+      .def("edit_double", static_cast<void (ConfigMap::*)(const std::string&, double)>(&ConfigMap::edit),
+             py::arg("key"), py::arg("value"))
+      .def("edit_str", static_cast<void (ConfigMap::*)(const std::string&, std::string)>(&ConfigMap::edit),
+             py::arg("key"), py::arg("value"))
+      .def("exist_u64", &ConfigMap::existU64, py::arg("key"))
+      .def("exist_i64", &ConfigMap::existI64, py::arg("key"))
+      .def("exist_double", &ConfigMap::existDouble, py::arg("key"))
+      .def("exist_str", &ConfigMap::existString, py::arg("key"))
+      .def("exist", &ConfigMap::exist, py::arg("key"))
+      .def("get_u64", &ConfigMap::getU64, py::arg("key"))
+      .def("get_i64", &ConfigMap::getI64, py::arg("key"))
+      .def("get_double", &ConfigMap::getDouble, py::arg("key"))
+      .def("get_str", &ConfigMap::getString, py::arg("key"))
+      .def("to_string", &ConfigMap::toString, py::arg("separator") = "\t", py::arg("new_line") = "\n")
+      .def("from_string", &ConfigMap::fromString, py::arg("src"), py::arg("separator") = "\t", py::arg("new_line") = "\n")
+      .def("clone_into", &ConfigMap::cloneInto, py::arg("dest"))
+      .def("load_from", &ConfigMap::loadFrom, py::arg("src"))
+      .def("to_file", &ConfigMap::toFile, py::arg("fname"), py::arg("separator") = ",", py::arg("new_line") = "\n")
+      .def("from_file", &ConfigMap::fromFile, py::arg("fname"), py::arg("separator") = ",", py::arg("new_line") = "\n")
+      .def("from_carg", [](INTELLI::ConfigMap& self, int argc, std::vector<std::string> argv) {
+            std::vector<const char*> c_args;
+            for (const auto& str : argv) {
+              c_args.push_back(str.c_str());
+            }
+            return self.fromCArg(argc, const_cast<char**>(c_args.data()));
+        }, py::arg("argc"), py::arg("argv"))
+      .def("try_u64", &ConfigMap::tryU64, py::arg("key"), py::arg("default_value") = 0, py::arg("show_warning") = false)
+      .def("try_i64", &ConfigMap::tryI64, py::arg("key"), py::arg("default_value") = 0, py::arg("show_warning") = false)
+      .def("try_string", &ConfigMap::tryString, py::arg("key"), py::arg("default_value") = "", py::arg("show_warning") = false)
+      .def("get_str_map", &ConfigMap::getStrMap)
+      .def("get_i64_map", &ConfigMap::getI64Map)
+      .def("get_double_map", &ConfigMap::getDoubleMap);
+
 }
