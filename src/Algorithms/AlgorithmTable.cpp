6cb2be6c67b56b5f535bd26810b3a11c4aa7c763
Lou Jun <126174019+iliujunn@users.noreply.github.com>
2024-11-28 17:58:02 +0800

    Revert "vamana algorithm"

--- src/Algorithms/AlgorithmTable.cpp
+++ src/Algorithms/AlgorithmTable.cpp
@@ -9,7 +9,6 @@
 #include <Algorithms/KDTree/KDTree.hpp>
 #include <Algorithms/KNN/KNNSearch.hpp>
 #include <Algorithms/LSH/LSHSearch.hpp>
-#include <Algorithms/Vamana/vamana.hpp>
 
 namespace CANDY_ALGO {
 AlgorithmTable::AlgorithmTable() {
@@ -18,6 +17,5 @@ AlgorithmTable::AlgorithmTable() {
   indexMap["HNSW"] = std::make_shared<HNSW>();
   indexMap["FlatGPU"] = std::make_shared<FlatGPUIndex>();
   indexMap["LSH"] = std::make_shared<LSHSearch>();
-  indexMap["Vamana"] = std::make_shared<Vamana>();
 }
 }  // namespace CANDY_ALGO
