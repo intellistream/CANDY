dc278aaa2dd0260713a5d47d36fd82730b2eff55
123kuangyu <96419373+123kuangyu@users.noreply.github.com>
2024-12-02 22:27:18 +0800

    Multimodal Test (#139)
    Co-authored-by: Lou Jun <126174019+iliujunn@users.noreply.github.com>

--- apps/Python/MultimodalRetrieval/main.py
+++ apps/Python/MultimodalRetrieval/main.py
@@ -1,26 +1,29 @@
 import sys
 import torch
 
+from Tools.RWTool import Read_ivecs, Read_fvecs, Write_ivecs, Calculate_recall
 from Tools.Client import DBClient
 
 from pycandy import ConfigMap
 from pycandy import DataLoaderTable
 
+
 def main():
 
     # 1.1 read config file.
-    defaultFile = "1.csv"
+    defaultFile = "config.csv"
     fileName = sys.argv[1] if len(sys.argv) >= 2 else defaultFile
     inMap = ConfigMap()
 
     if inMap.from_file(fileName):
-        print(f"[Python INFO] 1.1  Config loaded from file: {fileName}")
+        print(f"[Python INFO] 1.1  Config loaded from file: \033[1;32m{fileName}\033[0m")
     else:
         print("-" * 72)
-        print(str("1.1 Failed to load config from file: \"" + fileName + "\"").center(72));
+        print(str("1.1 Failed to load config from file: \"" + fileName + "\"").center(72))
         print("-" * 72)
         return -1
 
+    K=inMap.try_i64("ANNK", 1, True)
     # 1.2 load dataLoader and load data
     dataLoaderTable = DataLoaderTable()
     dataLoaderTag = inMap.try_string("dataLoaderTag", "random", True)
@@ -32,7 +35,7 @@ def main():
         print("-" * 72)
         return -1
     else:
-        print(f"[Python INFO] 1.2  Data loader: {dataLoaderTag} is ready")
+        print(f"[Python INFO] 1.2  Data loader: \033[1;32m{str(dataLoaderTag).upper()}dataloader\033[0m is ready")
 
     # set dataLoader`s param
     dataLoader.setConfig(inMap)
@@ -48,8 +51,9 @@ def main():
     print("=" * 50)
 
     # 2.1 Initialize vector database
-    client = DBClient(vecDim, 'hnswsearch', inMap)
-    print("[Python INFO] 2.1  The vector database has been successfully initialized")
+    indexTag = str(inMap.try_string("indexTag", "knn", True)).lower() + "search"
+    client = DBClient(vecDim, indexTag, inMap)
+    print(f"[Python INFO] 2.1  The vector database has been successfully initialized, we will use \033[1;32m{indexTag}\033[0m as our algorithm")
 
     # 2.2 load initial vector
     initialRows = inMap.try_i64("initialRows", 0, True)
@@ -80,8 +84,11 @@ def main():
     endRow = startRow + batchSize
     aRows = dataTensorStream.size(0)
 
+
     print("[Python INFO] 3.1  Streaming now !!!")
     processedOld = 0
+    batch_count=0
+    average_recall=0
     while startRow < aRows:
         sub_batch = dataTensorStream[startRow:endRow]
 
@@ -92,6 +99,7 @@ def main():
         # Update the row indices for the next batch
         startRow += batchSize
         endRow += batchSize
+        batch_query = queryTensor[startRow:endRow]
         if endRow > aRows:
             endRow = aRows
 
@@ -100,33 +108,50 @@ def main():
             startRow = aRows
         processed = startRow * 100.0 / aRows
         if processed - processedOld >= 1.0:
-
             print(f"Done {processed:.2f}% ({startRow}/{aRows})")
             processedOld = processed
-    print("[Python INFO] 3.2  Streaming has ended")
+
 
     # 3.3 Input the query and get the result
     result = client.get_batch_tensors(queryTensor)
 
     # 3.4 Get the gt
-    groundTruthTag = 0
+    groundTruthTag = inMap.try_i64("groundTruthTag", 0, True)
     if groundTruthTag == 0:
         print("[Python INFO] 3.4  Ground truth does not exist, so it`s time to create it")
         client_gt = DBClient(vecDim, 'knnsearch', inMap)
         client_gt.load_batch_tensor(dataTensorAll)
-        gt = client_gt.get_batch_tensors(queryTensor)
+        gt_path = inMap.try_string("groundtruthPath", "/gt_features.fvecs", True)
+        gt_tensor=torch.tensor((Read_fvecs(gt_path)))
+        gt = client_gt.get_batch_tensors(gt_tensor)
+        # result_path=f"/mnt/f/CANDY/apps/Python/MultimodalRetrieval/Datasets/recall{K}.fvecs"
+        # Write_ivecs(result_path, gt)
+
+    else:
+        print("[Python INFO] 3.4  Ground truth exists")
+        file_path = inMap.try_string("OfflinegroundtruthPath", f"/mnt/f/CANDY/apps/Python/MultimodalRetrieval/Datasets/recall100.fvecs", True)
+        gt = Read_ivecs(file_path)
 
     print("=" * 50)
 
     # 4.1 Calculate metrics "Recall"
-    positiveNum = 0
-    for i in range(len(result)):
-        if result[i] == gt[i]:
-            positiveNum += 1
-    recall = positiveNum / len(result)
-    print(f"[Python INFO] 4.1  RECALL = {recall:.6f}")
+    # positiveNum = 0
+    # for i in range(len(result)):
+    #     if result[i] == gt[i]:
+    #         positiveNum += 1
+    # recall = (positiveNum / len(result) * 100)
+    # (4181, 1)
+    # (N, 1, K)
+    # result: (N, K) gt[m] = K{indexID*K}
+    # gt:(N, K) (gt[m] ∩ result[m])
+    print("gt type: ",type(gt))
+    print("gt[0]: ",gt[0][:K])
+    print("result[0]: ", result[0])
+    recall = Calculate_recall(gt, result, K)
+    print(f"[Python INFO] 4.1  RECALL@{K} = \033[1;32m{recall*100:.4f}%\033[0m")
 
     return 0
 
 if __name__ == "__main__":
     main()
+
